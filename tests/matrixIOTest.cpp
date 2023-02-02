0b27cdc5d719fbb1bdf13f880456c5bd153a5ae3
Felix Neubauer <felix@neuby.de>
2023-02-02 16:56:26 +0100

    add matrixIO test

---
+++ tests/matrixIOTest.cpp
@@ -0,0 +1,23 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include "Configuration.hpp"
+#include "MatrixSolver.hpp"
+#include "matrixIO.hpp"
+
+BOOST_AUTO_TEST_SUITE(matrixIOTest)
+
+BOOST_AUTO_TEST_CASE(openData)
+{
+
+    A = MatrixXd(3, 3);
+    A << 1, 2, 3,
+        4, 5, 6,
+        7, 8, 9;
+  matrixIO::saveData("test.csv", A);
+  const Eigen::MatrixXd result = matrixIO::openData("test.csv", 3);
+
+
+  BOOST_TEST(A == result);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
