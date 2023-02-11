b5005dc33f906965e359a0557ffd87325b5097c1
Felix Neubauer <felix@neuby.de>
2023-02-11 16:22:45 +0100

    use clang format

--- tests/matrixIOTest.cpp
+++ tests/matrixIOTest.cpp
@@ -9,13 +9,12 @@ BOOST_AUTO_TEST_SUITE(matrixIOTest)
 BOOST_AUTO_TEST_CASE(openData)
 {
 
-    A = MatrixXd(3, 3);
-    A << 1, 2, 3,
-        4, 5, 6,
-        7, 8, 9;
+  A = MatrixXd(3, 3);
+  A << 1, 2, 3,
+      4, 5, 6,
+      7, 8, 9;
   matrixIO::saveData("test.csv", A);
   const Eigen::MatrixXd result = matrixIO::openData("test.csv", 3);
-
 
   BOOST_TEST(A == result);
 }
