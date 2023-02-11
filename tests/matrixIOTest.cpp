dd73248ef7d05bf07a8e76b85431efe12507c060
Felix Neubauer <felix@neuby.de>
2023-02-11 16:43:10 +0100

    update test

--- tests/matrixIOTest.cpp
+++ tests/matrixIOTest.cpp
@@ -12,20 +12,19 @@ BOOST_AUTO_TEST_CASE(openData)
 {
 
   auto expected = MatrixXd(3, 3);
-  expected << 1, 2, 3,
-      4, 5, 6,
-      7, 8, 9;
-  matrixIO::saveData("test.csv", expected);
-  auto result = matrixIO::openData("test.csv", 3);
-  BOOST_TEST(expected(0, 0) == result(0, 0));
-  BOOST_TEST(expected(0, 1) == result(0, 1));
-  BOOST_TEST(expected(0, 2) == result(0, 2));
-  BOOST_TEST(expected(1, 0) == result(1, 0));
-  BOOST_TEST(expected(1, 1) == result(1, 1));
-  BOOST_TEST(expected(1, 2) == result(1, 2));
-  BOOST_TEST(expected(2, 0) == result(2, 0));
-  BOOST_TEST(expected(2, 1) == result(2, 1));
-  BOOST_TEST(expected(2, 2) == result(2, 2));
+  expected << 0, -1, 2,
+      4, 100, 0,
+      -3, 2, 1.5;
+  auto actual = matrixIO::openData("testmatrix.csv", 3);
+  BOOST_TEST(expected(0, 0) == actual(0, 0));
+  BOOST_TEST(expected(0, 1) == actual(0, 1));
+  BOOST_TEST(expected(0, 2) == actual(0, 2));
+  BOOST_TEST(expected(1, 0) == actual(1, 0));
+  BOOST_TEST(expected(1, 1) == actual(1, 1));
+  BOOST_TEST(expected(1, 2) == actual(1, 2));
+  BOOST_TEST(expected(2, 0) == actual(2, 0));
+  BOOST_TEST(expected(2, 1) == actual(2, 1));
+  BOOST_TEST(expected(2, 2) == actual(2, 2));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
