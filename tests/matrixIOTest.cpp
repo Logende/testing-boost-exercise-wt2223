e393ba560b6d82e963960dd4acdc9e46fc5aa2f6
Felix Neubauer <felix@neuby.de>
2023-02-11 16:32:31 +0100

    fix test

--- tests/matrixIOTest.cpp
+++ tests/matrixIOTest.cpp
@@ -4,19 +4,29 @@
 #include "MatrixSolver.hpp"
 #include "matrixIO.hpp"
 
+using namespace Eigen;
+
 BOOST_AUTO_TEST_SUITE(matrixIOTest)
 
 BOOST_AUTO_TEST_CASE(openData)
 {
 
-  A = MatrixXd(3, 3);
-  A << 1, 2, 3,
+  expected = MatrixXd(3, 3);
+  expected << 1, 2, 3,
       4, 5, 6,
       7, 8, 9;
-  matrixIO::saveData("test.csv", A);
+  matrixIO::saveData("test.csv", expected);
   const Eigen::MatrixXd result = matrixIO::openData("test.csv", 3);
+   BOOST_TEST(expected(0, 0) == result(0, 0));
+   BOOST_TEST(expected(0, 1) == result(0, 1));
+   BOOST_TEST(expected(0, 2) == result(0, 2));
+   BOOST_TEST(expected(1, 0) == result(1, 0));
+   BOOST_TEST(expected(1, 1) == result(1, 1));
+   BOOST_TEST(expected(1, 2) == result(1, 2));
+   BOOST_TEST(expected(2, 0) == result(2, 0));
+   BOOST_TEST(expected(2, 1) == result(2, 1));
+   BOOST_TEST(expected(2, 2) == result(2, 2));
 
-  BOOST_TEST(A == result);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
